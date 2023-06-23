1323a1f0565017587c0d81890625e6f78878e765
oyama33 <56091711+oyama33@users.noreply.github.com>
2023-06-23 20:16:15 +0900

    MockするRepositoryに振る舞いを渡せるようにした

--- src/test/scala/com/poksha/sample/application/auth/AuthServiceSpec.scala
+++ src/test/scala/com/poksha/sample/application/auth/AuthServiceSpec.scala
@@ -4,19 +4,41 @@ import com.poksha.sample.domain.auth.{AuthUser, AuthUserId, AuthUserRepository}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-class AuthServiceSpec extends AnyFlatSpec with should.Matchers with AuthServiceSpecBase {
+class AuthServiceSpec
+    extends AnyFlatSpec
+    with should.Matchers
+    with AuthServiceSpecBase {
   "AuthServiceSpec" should "not create user if the email already exists" in {
+    implicit val authUserRepository
+        : AuthUserRepository = mockAuthUserRepository(
+      mockFindByEmail = {
+        case email if email == registeredEmail =>
+          Some(
+            AuthUser.EmailPasswordAuthUser(
+              AuthUserId.generate(),
+              "dummy",
+              "dummy"
+            )
+          )
+        case email if email == unregisteredEmail => None
+        case email =>
+          sys.error(
+            s"mockAuthUserRepository.findByEmail cannot handle the email [$email]. please set valid email as argument."
+          )
+      }
+    )
     val sut = new AuthService
+
     val actual = sut.create(
       CreateAuthUserCommand.CreatePasswordUser(registeredEmail, registeredPass)
     )
+
     actual.isLeft shouldBe true
   }
 }
 
-/**
- * AuthServiceSpec に必要な要素を定義するためのベース trait
- */
+/** AuthServiceSpec に必要な要素を定義するためのベース trait
+  */
 sealed trait AuthServiceSpecBase {
   val registeredEmail = "userEmail"
   val registeredPass = "userPass"
@@ -24,13 +46,18 @@ sealed trait AuthServiceSpecBase {
   val unregisteredEmail = "unregisteredEmail"
 
   // FIXME モックを作るライブラリを使うことで分かりやすくなるなら、ライブラリの導入を検討する
-  implicit val mockAuthUserRepository: AuthUserRepository = new AuthUserRepository {
-    override def find(id: AuthUserId): Option[AuthUser] = throw new NotImplementedError("find is not implemented")
-    override def findByEmail(email: String): Option[AuthUser] = email match {
-      case _ if email == registeredEmail   => Some(AuthUser.EmailPasswordAuthUser(AuthUserId.generate(), "dummy", "dummy"))
-      case _ if email == unregisteredEmail => None
-      case _                               => sys.error(s"mockAuthUserRepository.findByEmail cannot handle the email [$email]. please set valid email as argument.")
-    }
-    override def save(user: AuthUser): Either[String, AuthUser] = throw new NotImplementedError("save is not implemented")
+  def mockAuthUserRepository(
+      mockFindFunc: AuthUserId => Option[AuthUser] = _ =>
+        throw new NotImplementedError("mockFindFunc is not implemented"),
+      mockFindByEmail: String => Option[AuthUser] = _ =>
+        throw new NotImplementedError("mockFindByEmail is not implemented"),
+      mockSave: AuthUser => Either[String, AuthUser] = _ =>
+        throw new NotImplementedError("save is not implemented")
+  ): AuthUserRepository = new AuthUserRepository {
+    override def find(id: AuthUserId): Option[AuthUser] = mockFindFunc(id)
+    override def findByEmail(email: String): Option[AuthUser] = mockFindByEmail(
+      email
+    )
+    override def save(user: AuthUser): Either[String, AuthUser] = mockSave(user)
   }
 }
