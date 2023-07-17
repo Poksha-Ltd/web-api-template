13c8d02e714a89f06862bedfecb1f0446c3d1065
oyama33 <56091711+oyama33@users.noreply.github.com>
2023-07-17 23:35:12 +0900

    refactor: test用のSQLは不要になったので削除

--- docker/postgres/sql/create_test_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE test_authentication;
-
-\c test_authentication;
-CREATE TABLE auth_user(
-    id varchar(128) NOT NULL PRIMARY KEY,
-    email varchar(319),
-    hashed_password varchar(60)
-);
